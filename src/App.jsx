f99c27d9296c52fa3262c57eb7790a6049b57c12
Channa Maduranga <83961725+ChannaMaduranga@users.noreply.github.com>
2024-10-04 12:37:31 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'; // Change to HashRouter
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Change to HashRouter
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Components/Home/Home';
 import AllItems from './Components/AllItems/AllItems';
