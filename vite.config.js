08f0ee0b3362670c65276e2a589e95653160060a
Channa Maduranga <83961725+ChannaMaduranga@users.noreply.github.com>
2024-10-04 13:45:23 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/Camera-Rent-House/' : '/',
-  
-})
+  base: command === 'build' ? '/Camera-Rent-House/' : '/',  // Correct conditional for base path
+}));
